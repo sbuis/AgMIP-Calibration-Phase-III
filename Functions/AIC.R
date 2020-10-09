9e6d14fdfac371bc5b74c62d5360f5e354e3317c
Samuel Buis <samuel.buis@inrae.fr>
2020-10-09 15:28:30 +0200

    Added a function to compute AIC

---
+++ Functions/AIC.R
@@ -0,0 +1,21 @@
+#' @title Computes AIC for ordinary least squares
+#'
+#' @param obs_list List of observed values to use for parameter estimation
+#' (same as the one given to `estim_param` function)
+#' A `named list` (names = situations names) of data.frame containing
+#' one column named Date with the dates (Date or POSIXct format) of the different observations
+#' and one column per observed variables with either the measured values or NA, if
+#' the variable is not observed at the given date.
+#' @param crit_value Final value of the estimated criterion
+#' @param param_nb Number of estimated parameters
+#'
+#' @return Value of the AIC criterion
+#'
+
+AIC <- function(obs_list, crit_value, param_nb) {
+
+  n <- length(obs_list)
+  m <- sum(sapply(obs_list,function(x) sum(!is.na(x[,-1]))))  # total number of observations
+  return(n*log(2*pi)+n*log(crit_value)+m+param_nb)
+
+}
