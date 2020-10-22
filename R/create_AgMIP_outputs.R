ce9258782bc1c8aa6793cd36c3a399708d894ae9
sbuis <samuel.buis@inra.fr>
2020-10-22 16:21:18 +0200

    Better control of output format

--- R/create_AgMIP_outputs.R
+++ R/create_AgMIP_outputs.R
@@ -12,15 +12,16 @@
 #' @param model_options List of options for the Crop Model wrapper (see help of
 #' the Crop Model wrapper function used).
 #' 
+#' @param digits Number of digits to take into account for outputs printing format
+#' 
 #' @return A data.frame containing for each set of candidate parameters, the names of the parameters 
 #' and the initial and final values of the parameters, the OLS criterion and of AIC
 #' 
 
 create_AgMIP_outputs <- function(candidate_params, obs_list, optim_results, model_function, 
-                                 model_options) {
+                                 model_options, digits) {
   
   # Compute initial value of criterion and AIC (for the "best" repetition)
-  obs_list_ini <- obs_list
   model_results <- model_function(model_options = model_options, 
                                   param_values = optim_results$init_values[optim_results$ind_min_crit,], 
                                   sit_names = names(obs_list),
@@ -32,14 +33,15 @@ create_AgMIP_outputs <- function(candidate_params, obs_list, optim_results, mode
   final_aic <- AIC(obs_list, optim_results$min_crit_value, param_nb=length(candidate_params))
   
   # Gather optim_resultsults
-  df <- data.frame(param_names=NA, init_values=NA, 
-                   ini_crit=init_crit, ini_aic=init_aic, 
-                   final_values=NA, final_crit=optim_results$min_crit_value, 
-                   final_aic=final_aic)
-  df$init_values[1]=list(optim_results$init_values[optim_results$ind_min_crit,])
-  df$final_values[1]=list(optim_results$final_values)
-  df$param_names[1]=list(candidate_params)
-  
+  df <- data.frame(param_names=paste(candidate_params,collapse = ", "), 
+                   init_values=paste(format(optim_results$init_values[optim_results$ind_min_crit,], 
+                                            scientific=FALSE, digits=digits, nsmall=2), collapse=", "), 
+                   ini_crit=format(init_crit, scientific=FALSE, digits=digits, nsmall=2), 
+                   ini_aic=format(init_aic, scientific=FALSE, digits=digits, nsmall=2), 
+                   final_values=paste(format(optim_results$final_values, scientific=FALSE, digits=digits, nsmall=2),collapse=", "), 
+                   final_crit=format(optim_results$min_crit_value, scientific=FALSE, digits=digits, nsmall=2), 
+                   final_aic=format(final_aic, scientific=FALSE, digits=digits, nsmall=2))
+
   return(df)
   
 }
